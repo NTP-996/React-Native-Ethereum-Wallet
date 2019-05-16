c9c1fd716f2933964bf88d433dbb0119c6c30485
NTPhuong <thaiphuongn1@gmail.com>
2019-05-17 01:31:23 +0700

    Receive Screen

--- src/components/CreateWalletScreen.js
+++ src/components/CreateWalletScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, AsyncStorage } from 'react-native';
 import { Container, Content, Segment, Text, Icon, Button, Header, Left, Body, Title, Right, Form, Textarea, Input, Item } from 'native-base';
+import RNSecureKeyStore, {ACCESSIBLE} from "react-native-secure-key-store";
 
 import bip39 from 'react-native-bip39';
 import hdkey from 'hdkey';
@@ -27,19 +28,52 @@ export default class CreateWalletScreen extends Component {
         });
     }
 
+    _storeData = async (wallet, privateKey) => {
+        try {
+            // Get your old Wallet listing information
+            const wallets = JSON.parse(await AsyncStorage.getItem('WALLETS')) || [];
+            // Add to existing Wallet list
+            wallets.push(wallet);
+            // Saving wallet listing information
+            await AsyncStorage.setItem('WALLETS', JSON.stringify(wallets));
+            // Storing private keys in a secure zone
+            await RNSecureKeyStore.set(wallet.address, privateKey, {accessible: ACCESSIBLE.ALWAYS_THIS_DEVICE_ONLY});
+        } catch (error) {
+            // Error saving data
+            console.log(error);
+        }
+    };
+
     _createWallet = async () => {
+
+        // 1. Seed calculation in mnemonic
         const seed = bip39.mnemonicToSeed(this.state.mnemonic);
-        // Master Key Generation (HDPrivateKey)
+
+        // 2. Master key generation in seed (HDPrivateKey)
         const root = hdkey.fromMasterSeed(seed);
-        // Ethereum private key generation
+
+        // 3. Derived child private key generation (derived)
         const xPrivKey = root.derive("m/44'/60'/0'/0/0");
         const privKey = root.privateKey.toString('hex');
-        // Ethereum address generation
+
+        // 4. Calculate Ethernet address from public key
         const pubKey = ethUtil.privateToPublic(xPrivKey._privateKey);
         let address = ethUtil.publicToAddress(pubKey).toString('hex');
-        // Convert to Ethereum EIP-55 checksum address
         address = ethUtil.toChecksumAddress(address);
-        alert(address);
+       
+        // Generate wallet information to save
+        const wallet = {
+            name: 'Ethereum',
+            coinType: 'ETH',
+            symbol: 'ETH',
+            address
+        }
+
+        // Save
+        await this._storeData(wallet, privKey);
+
+        // Back to wallet list screen
+        this.props.navigation.goBack();
     }
 
     render() {
